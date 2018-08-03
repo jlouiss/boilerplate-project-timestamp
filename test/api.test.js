6a6ab23d5a11dbc6b8f96aaed30e7361e6987b35
Jean Louis Salbego <jlsalbego@protonmail.com>
2018-08-03 22:28:26 +0200

    I got it to work with chai, too

--- test/api.test.js
+++ test/api.test.js
@@ -1,5 +1,10 @@
 const request = require('supertest')
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+
 const app = require('../app')
+
+chai.use(chaiHttp)
 
 describe('Learning TDD', () => {
 
@@ -7,6 +12,12 @@ describe('Learning TDD', () => {
     test('It should work, otherwise I would get a bit frustrated', async () => {
       const response = await request(app).get('/');
       expect(response.statusCode).toBe(200);
+    })
+
+    test('It should work with chai, too', async () => {
+      const response = await chai.request(app).get('/');
+      expect(response.status).toEqual(200);
+      expect(response.body).toBeDefined();
     })
   })
 
